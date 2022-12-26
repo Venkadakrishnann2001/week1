e56f23a45a3a582027b4f1f7d84fdd424d1c6d54
Venkadakrishnann2001 <111949601+Venkadakrishnann2001@users.noreply.github.com>
2022-12-26 22:06:46 +0530

    Add files via upload

---
+++ week-1-javascript-Venkadakrishnann2001-main/getUserInput/js/getuserInput.js
@@ -0,0 +1,16 @@
+
+// Gets user input
+var Name = prompt("What is your name?");
+var roll = prompt("enter your roll no? ");
+var fnumber=parseInt(prompt("enter your fist number?"))
+var snumber=parseInt(prompt("enter your second number?"))
+// Uses user input to print out information
+var add=fnumber+snumber;
+document.getElementById("a").innerHTML=" your name: "+Name;
+document.getElementById("b").innerHTML=" your roll no: "+roll;
+document.getElementById("c").innerHTML=" your first number: "+fnumber;
+document.getElementById("d").innerHTML=" your secondnumber: "+snumber;
+document.getElementById("e").innerHTML=" total is: "+add;
+
+// Prints out the variable type
+
